f9350d412d1edbdfa820f850aef7be7b14fb00e7
bobbyZ94 <r.zioltkowski@gmail.com>
2023-01-04 20:46:04 +0100

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script>
+	import '../app.css';
+	const year = new Date().getFullYear();
+</script>
+
+<div
+	class="h-screen flex flex-col items-center justify-between bg-zinc-800 text-gray-100 font-montserrat"
+>
+	<nav class="bg-red-500">
+		<a href="/">About</a>
+		<!-- <a href="/projects">Projects</a>
+		<a href="/blog">Blog</a>
+		<a href="/contact">Contact</a> -->
+	</nav>
+	<slot />
+	<footer>
+		<a href="/impressum">Impressum</a>
+		<a href="/legal">Legal</a>
+		<div>Robzio © {year}</div>
+	</footer>
+</div>
