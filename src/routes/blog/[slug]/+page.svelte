3efd49ec43d968b7cc7f68e54dbca8aa35fb5106
bobbyZ94 <r.zioltkowski@gmail.com>
2023-02-17 17:29:05 +0100

    feat: updated tailwind and tailwind/typography packages; fix: removed <br> margins cause of breaking styles in firefox

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -19,8 +19,7 @@
 		{ languageName: 'html', languageHighlite: xml },
 		{ languageName: 'css', languageHighlite: css },
 		{ languageName: 'javascript', languageHighlite: javascript },
-		{ languageName: 'typescript', languageHighlite: typescript },
-		{ languageName: 'svelte' }
+		{ languageName: 'typescript', languageHighlite: typescript }
 	];
 
 	async function saveToClipboard(code) {
@@ -47,10 +46,13 @@
 	</div>
 
 	<div
-		class="z-20 max-w-4xl p-5 px-5 m-5 overflow-hidden leading-7 tracking-wider prose-sm break-words prose-code:text-sm md:prose-code:text-base prose-table:my-2 prose-pre:p-0 prose-h1:text-2xl prose-h2:text-xl prose-h3:text-lg md:prose-h2:text-3xl md:prose-h3:text-2xl md:prose-h1:text-4xl rounded-xl bg-zinc-800 md:p-10 md:px-10 md:prose-base prose-ul:list-disc prose-ol:list-decimal hover:prose-a:underline prose-a:text-orange-500 opacity-95 prose-p:my-2 prose-headings:my-7"
+		class="z-20 max-w-4xl p-5 px-5 m-5 overflow-hidden tracking-wider leading-7 prose-sm break-words prose-code:text-sm md:prose-code:text-base prose-table:my-2 prose-pre:p-0 prose-h1:text-2xl prose-h2:text-xl prose-h3:text-lg md:prose-h2:text-3xl md:prose-h3:text-2xl md:prose-h1:text-4xl rounded-xl bg-zinc-800 md:p-10 md:px-10 md:prose-base prose-ul:list-disc prose-ol:list-decimal hover:prose-a:underline prose-a:text-orange-500 opacity-95 prose-p:my-2 prose-headings:my-7"
 	>
-		<div class="mb-10 text-2xl font-semibold md:text-5xl">
+		<div class="mb-5 text-2xl font-semibold md:text-5xl">
 			<span class="bg-half-color">{blogEntry.title}</span>
+		</div>
+		<div class="italic w-full flex justify-end">
+			Last edit: {new Date(blogEntry.date).toLocaleDateString('de-DE')}
 		</div>
 		<div class="h-[10rem] md:h-[20rem]">
 			<img
@@ -75,30 +77,39 @@
 							<CopyFile size={24} />
 						</div>
 					</div>
-					{#if block.blockType === 'code svelte'}
-						<HighlightSvelte
-							code={block.code}
-							let:highlighted
-						>
-							<LineNumbers
-								{highlighted}
-								hideBorder
-							/>
-						</HighlightSvelte>
-					{:else}
-						<Highlight
-							language={language.languageHighlite}
-							code={block.code}
-							let:highlighted
-						>
-							<LineNumbers
-								{highlighted}
-								hideBorder
-							/>
-						</Highlight>
-					{/if}
+					<Highlight
+						language={language.languageHighlite}
+						code={block.code}
+						let:highlighted
+					>
+						<LineNumbers
+							{highlighted}
+							hideBorder
+						/>
+					</Highlight>
 				{/if}
 			{/each}
+			{#if block.blockType === 'code svelte'}
+				<div class="flex items-center justify-between gap-3 px-5 py-2 translate-y-2 bg-gray-900">
+					<div class="break-all">{block.filename}</div>
+					<!-- svelte-ignore a11y-click-events-have-key-events -->
+					<div
+						on:click={saveToClipboard(block.code)}
+						class="cursor-pointer hover:scale-105"
+					>
+						<CopyFile size={24} />
+					</div>
+				</div>
+				<HighlightSvelte
+					code={block.code}
+					let:highlighted
+				>
+					<LineNumbers
+						{highlighted}
+						hideBorder
+					/>
+				</HighlightSvelte>
+			{/if}
 		{/each}
 	</div>
 </div>
