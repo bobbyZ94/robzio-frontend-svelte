15f388d8c0648742d52ab17533450b80349fb0bb
bobbyZ94 <r.zioltkowski@gmail.com>
2023-02-18 12:08:33 +0100

    feat: added custom 404 page with styling

---
+++ src/routes/+error.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<div class="flex items-center justify-center flex-grow">
+	<div>
+		{$page.error.message}. This page does not exists! 404!
+		<div>
+			<a
+				href="/"
+				class="underline"
+			>
+				Go back to the frontpage ...</a
+			>
+		</div>
+	</div>
+</div>
